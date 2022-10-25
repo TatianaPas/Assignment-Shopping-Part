d82cad9948b6c9429b9b90049753a3c03e2beadf
Tatiana <tatjana.pasechnik@gmail.com>
2022-10-25 21:09:10 +1300

    Shop page

--- FullStackAssignemntT/FullStackAssignemntT/Controllers/HomeController.cs
+++ FullStackAssignemntT/FullStackAssignemntT/Controllers/HomeController.cs
@@ -1,21 +1,56 @@
-﻿using FullStackAssignemntT.Models;
+﻿using FullStackAssignemntT.Data;
+using FullStackAssignemntT.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace FullStackAssignemntT.Controllers
 {
     public class HomeController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+
+
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        //25.10 Tatiana-shop page
+        public async Task<IActionResult> Shop()
+        {
+            IEnumerable<Product> products = await _context.ShopProducts.Include(p => p.Category).Include(p => p.Size).ToListAsync();
+            return View(products);
+        }
+
+        //25.10 Tatiana - product details page
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.ShopProducts == null)
+            {
+                return NotFound();
+            }
+
+            ShoppingCart shoppingCart = new()
+            {
+                Count = 1,
+                Product = await _context.ShopProducts
+                .Include(p => p.Category)
+                .Include(p => p.Size)
+                .FirstOrDefaultAsync(m => m.Id == id),
+            };
+
+            return View(shoppingCart);
         }
 
         public IActionResult Privacy()
---
+++ FullStackAssignemntT/FullStackAssignemntT/Models/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FullStackAssignemntT.Models
+{
+    public class ShoppingCart
+    {
+        public Product Product { get; set; }
+        [Range(1, 10, ErrorMessage = "Please add correct amount of product")]
+        public int Count { get; set; }
+
+        public ShoppingCart()
+        {
+            Count = 1;
+        }
+    }
+}
