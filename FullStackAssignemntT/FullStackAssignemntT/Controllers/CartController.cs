3e6f5a44ad0dd8c5ea299a44e596103cf8ab1f91
Tatiana <tatjana.pasechnik@gmail.com>
2022-10-27 22:04:10 +1300

    stripe payment

--- FullStackAssignemntT/FullStackAssignemntT/Controllers/CartController.cs
+++ FullStackAssignemntT/FullStackAssignemntT/Controllers/CartController.cs
@@ -5,6 +5,7 @@ using FullStackAssignemntT.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Stripe.Checkout;
 using System.Security.Claims;
 
 namespace FullStackAssignemntT.Controllers
@@ -113,9 +114,76 @@ namespace FullStackAssignemntT.Controllers
                 _context.SaveChanges();
             }
 
-            _context.RemoveRange((ShoppingCartVM.CartList));
+
+            //27.10 Tatiana Stripe pyment settings from stripe page https://stripe.com/docs/checkout/quickstart
+
+            var domain = "https://localhost:44360/";
+            var options = new SessionCreateOptions
+            {
+                PaymentMethodTypes = new List<string>
+                {
+                  "card",
+                },
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                SuccessUrl = domain + $"cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+                CancelUrl = domain + $"cart/index",
+            };
+
+            foreach (var item in ShoppingCartVM.CartList)
+            {
+                //27.10 Tatiana - display product details like name and quantity on checkout page
+                var sessionLineItem = new SessionLineItemOptions
+                {
+                    
+                    PriceData = new SessionLineItemPriceDataOptions
+                    {
+                        UnitAmount = (long)(item.Product.ListPrice * 100),
+                        Currency = "nzd",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.Name
+                        },
+
+                    },
+                    Quantity = item.Count,
+                };
+                options.LineItems.Add(sessionLineItem);
+
+            }
+
+            var service = new SessionService();
+            Session session = service.Create(options);
+            //add stripe session and payment intent ID to database
+            ShoppingCartVM.OrderHeader.SessionId = session.Id;
+            
             _context.SaveChanges();
-            return RedirectToAction("Shop", "Home");
+
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+            
+        }
+
+
+        //27.10 Tatiana  - check if payment is sucessfull from stripe card pauyment
+        public async Task<IActionResult> OrderConfirmation(int id)
+        {
+            OrderHeader orderHeader = await _context.ShopOrderHeaders.FirstOrDefaultAsync(s=>s.Id == id);
+            var service = new SessionService();
+            Session session = service.Get(orderHeader.SessionId);
+            //check Stripe status
+            if(session.PaymentStatus.ToLower()=="paid")
+            {
+                orderHeader.PaymentIntentId = session.PaymentIntentId;
+                orderHeader.OrderStatus = StaticDetails.StatusApproved;
+                orderHeader.PaymentStatus = StaticDetails.PaymentStatusApproved;
+                _context.SaveChanges();
+            }
+            List<ShoppingCart> shoppicCart = _context.ShopShoppingCart.Where(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
+            _context.RemoveRange(shoppicCart);
+            _context.SaveChanges();
+            return View(id);
         }
 
 
